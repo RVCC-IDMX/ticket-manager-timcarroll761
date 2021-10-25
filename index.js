7ec13ea539f49ebc7d33fe686f33756184801461
timcarroll761 <timcarroll761@gmail.com>
2021-10-24 22:23:14 -0400

    Handle errors

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const chalk = require('chalk');
 const TicketManager = require('./ticketManager');
 const EmailService = require('./emailService');
 const DatabaseService = require('./databaseService');
@@ -7,11 +6,16 @@ const ticketManager = new TicketManager(3);
 const emailService = new EmailService();
 const databaseService = new DatabaseService();
 
-const { log } = console;
-
 ticketManager.on('buy', (email, price, timestamp) => {
   emailService.send(email);
   databaseService.save(email, price, timestamp);
 });
 
+ticketManager.on('error', (error) => {
+  console.error(`Gracefully handling our error: ${error}`);
+});
+
+ticketManager.buy('test@email.com', 10);
+ticketManager.buy('test@email.com', 10);
+ticketManager.buy('test@email.com', 10);
 ticketManager.buy('test@email.com', 10);
