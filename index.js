1ad6d3ce27c3aee2e089bd2b0a1a3d2cb00680e9
timcarroll761 <timcarroll761@gmail.com>
2021-10-24 22:37:11 -0400

    Finish the assignment

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const chalk = require('chalk');
+
+const { log } = console;
 const TicketManager = require('./ticketManager');
 const EmailService = require('./emailService');
 const DatabaseService = require('./databaseService');
@@ -15,7 +18,54 @@ ticketManager.on('error', (error) => {
   console.error(`Gracefully handling our error: ${error}`);
 });
 
-ticketManager.buy('test@email.com', 10);
-ticketManager.buy('test@email.com', 10);
-ticketManager.buy('test@email.com', 10);
-ticketManager.buy('test@email.com', 10);
+log(
+  chalk.green.bgMagenta(
+    `We have ${ticketManager.listenerCount(
+      'buy'
+    )} listener(s) for the buy event`
+  )
+);
+log(
+  chalk.red.bgYellow(
+    `We have ${ticketManager.listenerCount(
+      'error'
+    )} listener(s) for the error event`
+  )
+);
+
+const onBuy = () => {
+  log('I will be removed soon');
+};
+
+ticketManager.on('buy', onBuy);
+
+log(
+  chalk.green.bgMagenta(
+    `\nWe added a new event listener bringing our total count for the buy event to: ${ticketManager.listenerCount(
+      'buy'
+    )}`
+  )
+);
+ticketManager.buy('test@email', 20);
+
+ticketManager.off('buy', onBuy);
+
+log(
+  chalk.green.bgMagenta(
+    `\nWe now have: ${ticketManager.listenerCount(
+      'buy'
+    )} listener(s) for the buy event`
+  )
+);
+ticketManager.buy('test@email', 20);
+
+ticketManager.removeAllListeners('buy');
+console.log(
+  chalk.green.bgMagenta(
+    `\nWe have ${ticketManager.listenerCount(
+      'buy'
+    )} listeners for the buy event`
+  )
+);
+ticketManager.buy('test@email', 20);
+log('The last ticket was bought');
